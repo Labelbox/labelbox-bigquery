79ef49ea5fb13bd59e67c8b0c69351fc629fd8f7
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-01-26 13:11:57 -0500

    Update client.py

--- labelboxbigquery/client.py
+++ labelboxbigquery/client.py
@@ -38,142 +38,6 @@ class Client:
         self.lb_client = labelboxClient(lb_api_key, endpoint=lb_endpoint, enable_experimental=lb_enable_experimental, app_url=lb_app_url)
         bq_creds = service_account.Credentials.from_service_account_file(google_key) if google_key else None
         self.bq_client = bigquery.Client(project=google_project_name, credentials=bq_creds)
-    
-    def _sync_metadata_fields(self, bq_table_id, metadata_index={}):
-        """ Ensures Labelbox's Metadata Ontology has all necessary metadata fields given a metadata_index
-        Args:
-            bq_table_id     :   Required (str) - Table ID structured in the following schema: google_project_name.dataset_name.table_name
-            metadata_index  :   Optional (dict) - Dictionary where {key=column_name : value=metadata_type} - metadata_type must be one of "enum", "string", "datetime" or "number"
-        Returns:
-            True if the sync is successful, False if not
-        """
-        # Get your metadata ontology
-        lb_mdo = self.lb_client.get_data_row_metadata_ontology()
-        bq_table = self.bq_client.get_table(bq_table_id)
-        # Convert your meatdata_index values from strings into labelbox.schema.data_row_metadata.DataRowMetadataKind types
-        conversion = {"enum" : DataRowMetadataKind.enum, "string" : DataRowMetadataKind.string, "datetime" : DataRowMetadataKind.datetime, "number" : DataRowMetadataKind.number}
-        # Grab all the metadata field names
-        lb_metadata_names = [field['name'] for field in lb_mdo._get_ontology()]
-        # Iterate over your metadata_index, if a metadata_index key is not an existing metadata_field, then create it in Labelbox
-        if metadata_index:
-            for column_name in metadata_index.keys():
-                metadata_type = metadata_index[column_name]
-                if metadata_type not in conversion.keys():
-                    print(f'Error: Invalid value for metadata_index field {column_name}: {metadata_type}')
-                    return False
-                if column_name not in lb_metadata_names:
-                    # For enum fields, grab all the unique values from that column as a list
-                    if metadata_type == "enum":
-                        query_job = self.bq_client.query(f"""SELECT DISTINCT {column_name} FROM {bq_table.table_id}""")
-                        enum_options = [row.values()[0] for row in query_job]
-                    else:
-                        enum_options = []
-                    lb_mdo.create_schema(name=column_name, kind=conversion[metadata_type], options=enum_options)
-                    lb_mdo = self.lb_client.get_data_row_metadata_ontology()
-                    lb_metadata_names = [field['name'] for field in lb_mdo._get_ontology()]
-        # Iterate over your metadata_index, if a metadata_index key is not an existing column name, then create it in BigQuery  
-        if metadata_index:
-            column_names = [schema_field.name for schema_field in bq_table.schema]
-            for metadata_field_name in metadata_index.keys():
-                if metadata_field_name not in column_names:
-                    original_schema = bq_table.schema
-                    new_schema = original_schema[:]
-                    new_schema.append(bigquery.SchemaField(metadata_field_name, "STRING"))
-                    bq_table.schema = new_schema
-                    bq_table = self.bq_client.update_table(bq_table, ["schema"])                   
-        # Track data rows loaded from BigQuery
-        if "lb_integration_source" not in lb_metadata_names:
-            lb_mdo.create_schema(name="lb_integration_source", kind=DataRowMetadataKind.string)
-        return True
-
-    def __get_metadata_schema_to_name_key(self, lb_mdo:labelbox.schema.data_row_metadata.DataRowMetadataOntology, divider="///", invert=False):
-        """ Creates a dictionary where {key=metadata_schema_id: value=metadata_name_key} 
-        - name_key is name for all metadata fields, and for enum options, it is "parent_name{divider}child_name"
-        Args:
-            lb_mdo              :   Required (labelbox.schema.data_row_metadata.DataRowMetadataOntology) - Labelbox metadata ontology
-            divider             :   Optional (str) - String separating parent and enum option metadata values
-            invert              :   Optional (bool) - If True, will make the name_key the dictionary key and the schema_id the dictionary value
-        Returns:
-            Dictionary where {key=metadata_schema_id: value=metadata_name_key}
-        """
-        lb_metadata_dict = lb_mdo.reserved_by_name
-        lb_metadata_dict.update(lb_mdo.custom_by_name)
-        metadata_schema_to_name_key = {}
-        for metadata_field_name in lb_metadata_dict:
-            if type(lb_metadata_dict[metadata_field_name]) == dict:
-                metadata_schema_to_name_key[lb_metadata_dict[metadata_field_name][next(iter(lb_metadata_dict[metadata_field_name]))].parent] = str(metadata_field_name)
-                for enum_option in lb_metadata_dict[metadata_field_name]:
-                    metadata_schema_to_name_key[lb_metadata_dict[metadata_field_name][enum_option].uid] = f"{str(metadata_field_name)}{str(divider)}{str(enum_option)}"
-            else:
-                metadata_schema_to_name_key[lb_metadata_dict[metadata_field_name].uid] = str(metadata_field_name)
-        return_value = metadata_schema_to_name_key if not invert else {v:k for k,v in metadata_schema_to_name_key.items()}
-        return return_value
-    
-    def __batch_create_data_rows(self, client, dataset, global_key_to_upload_dict, skip_duplicates=True, batch_size=20000):
-        """ Checks to make sure no duplicate global keys are uploaded before batch uploading data rows
-        Args:
-            client                      : Required (labelbox.client.Client) : Labelbox Client object
-            dataset                     : Required (labelbox.dataset.Dataset) : Labelbox Dataset object
-            global_key_to_upload_dict   : Required (dict) : Dictionary where {key=global_key : value=data_row_dict to-be-uploaded to Labelbox}
-            skip_duplicates             : Optional (bool) - If True, will skip duplicate global_keys, otherwise will generate a unique global_key with a suffix "_1", "_2" and so on
-            batch_size                  : Optional (int) : Upload batch size, 20,000 is recommended
-        Returns:
-            A concatenated list of upload results for all batch uploads
-        """
-        def __check_global_keys(client, global_keys):
-            """ Checks if data rows exist for a set of global keys
-            Args:
-                client                  : Required (labelbox.client.Client) : Labelbox Client object
-                global_keys             : Required (list(str)) : List of global key strings
-            Returns:
-                True if global keys are available, False if not
-            """
-            query_keys = [str(x) for x in global_keys]
-            # Create a query job to get data row IDs given global keys
-            query_str_1 = """query get_datarow_with_global_key($global_keys:[ID!]!){dataRowsForGlobalKeys(where:{ids:$global_keys}){jobId}}"""
-            query_job_id = client.execute(query_str_1, {"global_keys":global_keys})['dataRowsForGlobalKeys']['jobId']
-            # Get the results of this query job
-            query_str_2 = """query get_job_result($job_id:ID!){dataRowsForGlobalKeysResult(jobId:{id:$job_id}){data{
-                            accessDeniedGlobalKeys\ndeletedDataRowGlobalKeys\nfetchedDataRows{id}\nnotFoundGlobalKeys}jobStatus}}"""
-            res = client.execute(query_str_2, {"job_id":query_job_id})['dataRowsForGlobalKeysResult']['data']
-            return res
-        global_keys_list = list(global_key_to_upload_dict.keys())
-        payload = __check_global_keys(client, global_keys_list)
-        loop_counter = 0
-        if payload:
-            while len(payload['notFoundGlobalKeys']) != len(global_keys_list):
-                loop_counter += 1
-                if payload['deletedDataRowGlobalKeys']:
-                    client.clear_global_keys(payload['deletedDataRowGlobalKeys'])
-                    payload = __check_global_keys(client, global_keys_list)
-                    continue
-                if payload['fetchedDataRows']:
-                    for i in range(0, len(payload['fetchedDataRows'])):
-                        if payload['fetchedDataRows'][i] != "":
-                            if skip_duplicates:
-                                global_key = str(global_keys_list[i])
-                                del global_key_to_upload_dict[str(global_key)]
-                            else:
-                                global_key = str(global_keys_list[i])
-                                new_upload_dict = global_key_to_upload_dict[str(global_key)]
-                                del global_key_to_upload_dict[str(global_key)]
-                                new_global_key = f"{global_key}_{loop_counter}"
-                                new_upload_dict['global_key'] = new_global_key
-                                global_key_to_upload_dict[new_global_key] = new_upload_dict
-                    global_keys_list = list(global_key_to_upload_dict.keys())
-                    payload = __check_global_keys(client, global_keys_list)
-        upload_list = list(global_key_to_upload_dict.values())
-        upload_results = []
-        for i in range(0,len(upload_list),batch_size):
-            batch = upload_list[i:] if i + batch_size >= len(upload_list) else upload_list[i:i+batch_size]
-            task = dataset.create_data_rows(batch)
-            errors = task.errors
-            if errors:
-                print(f'Data Row Creation Error: {errors}')
-                return errors
-            else:
-                upload_results.extend(task.result)
-        return upload_results   
 
     def create_data_rows_from_table(self, bq_table_id, lb_dataset, row_data_col, global_key_col=None, external_id_col=None, metadata_index={}, attachment_index={}, skip_duplicates=False):
         """ Creates Labelbox data rows given a BigQuery table and a Labelbox Dataset
@@ -189,10 +53,12 @@ class Client:
         Returns:
             List of errors from data row upload - if successful, is an empty list
         """
-        # Sync metadata index keys with metadata ontology
-        check = self._sync_metadata_fields(bq_table_id, metadata_index)
-        if not check:
-          return None
+        # Sync metadata index keys with metadata ontology   
+        table = sync_metadata_fields(
+            client=self.lb_client, table=self.bq_client.get_table(bq_table_id)  , get_columns_function=connector.get_columns_function, 
+            add_column_function=connector.add_column_function, get_unique_values_function=connector.get_unique_values_function, 
+            metadata_index=metadata_index, verbose=verbose, extra_client=self.bq_client
+        )
         # Create a metadata_schema_dict where {key=metadata_field_name : value=metadata_schema_id}
         lb_mdo = self.lb_client.get_data_row_metadata_ontology()
         metadata_name_key_to_schema = self.__get_metadata_schema_to_name_key(lb_mdo, invert=True)
